9ee459a9160607503e7511b419132a1d9211255f
hosm100 <138240259+hosm100@users.noreply.github.com>
2024-05-10 21:51:56 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 const divSelectorsToRemove = [
   'div.home-interleave-divider',
-  'div[data-testid="home-page-game-grid"]'
+  'div[data-testid="home-page-game-grid"]',
+  'div.wide-game-tile-carousel',
 ];
 
 function removeDivs() {
@@ -10,6 +11,26 @@ function removeDivs() {
       element.remove();
     });
   });
+
+  const spanElements = document.querySelectorAll('span');
+  spanElements.forEach(span => {
+    if (span.textContent.includes("Today's Picks") || span.textContent.includes("A curated selection of daily highlights")) {
+      span.remove();
+    }
+  });
+
+// Remove sponsored section heading and carousel
+const sponsoredDivs = document.querySelectorAll('div.game-sort-header-container');
+sponsoredDivs.forEach(div => {
+  const heading = div.querySelector('h2');
+  if (heading && heading.textContent.includes('Sponsored')) {
+    const carousel = div.nextElementSibling; // Get the next element which should be the carousel
+    if (carousel && carousel.classList.contains('game-carousel')) {
+      carousel.remove();
+    }
+    div.remove();
+  }
+});
 }
 
 // Mutation observer to handle dynamically added content
